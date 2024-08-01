049c57f6da67688ad416b2cc4490b9617359be2b
pavi-2505 <pavithramariyappan525@gmail.com>
2024-08-01 14:37:48 +0200

    2

--- JenExe/src/test/java/newPjct/Pjct1Test.java
+++ JenExe/src/test/java/newPjct/Pjct1Test.java
@@ -1,5 +1,7 @@
 package newPjct;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Pjct1Test {
@@ -7,6 +9,7 @@ public class Pjct1Test {
 	@Test
 	public void pjct1Test()
 	{
-		System.out.println("hello world");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
 	}
 }
